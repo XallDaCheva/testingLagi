2ab272b11f709c6031055c6ab31e0d1842ab838e
xall-gitautoco <xallgitautopilot0@gmail.com>
2024-12-21 16:48:01 +0700

    tesNgubah

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "tes_db",
+  database: "ngubah_ngubah_disini_________",
 });
 
 db.connect((err) => {
